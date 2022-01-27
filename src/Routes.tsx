8192b1f5b1d7b1976471f1316cf496ad3680c216
Leandro Santo <leandro.santo@outlook.com>
2022-01-27 00:11:43 -0300

    criando rotas e estilos

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SideBar } from './components/SideBar';
 
 import { Home } from './pages/Home';
 import { NewClient } from './pages/NewClient';
 
+import './global.css'
+
 export function AppRoutes() {
   return(
     <Router>
+      <SideBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/newclient" element={<NewClient />} />
---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+import './styles.css';
+
+export function SideBar(){
+return(
+  <div className="sideBarContainer">
+    <nav>
+      <Link to='/'>Dashboard</Link>
+
+      <Link to='/newclient'>Novo Cliente</Link>
+    </nav>
+  </div>
+  )
+}
